b349f675996d0a48b850e05271f2ecfc4dbb6216
Faiz Khan <pfaizkhan95@gmail.com>
2026-01-31 11:01:42 +0530

    file position change

--- horizon-automation-suite-main/src/lib/firebase.ts
+++ horizon-automation-suite-main/src/lib/firebase.ts
@@ -5,10 +5,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyD-yxxre4gSYxlOdOajqRruip82xwz9_aQ",
   authDomain: "urbanium-8db16.firebaseapp.com",
   projectId: "urbanium-8db16",
-  storageBucket: "urbanium-8db16.firebasestorage.app",
+  storageBucket: "urbanium-8db16.appspot.com",
   messagingSenderId: "445154266400",
   appId: "1:445154266400:web:225edda3f8d6a759c59b45",
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
