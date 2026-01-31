cd90a08c9aa1c8d62f8544e5dda46116ddce0c7a
Faiz Khan <pfaizkhan95@gmail.com>
2026-01-31 11:27:37 +0530

    deploement file added

--- horizon-automation-suite-main/src/lib/firebase.ts
+++ horizon-automation-suite-main/src/lib/firebase.ts
@@ -1,13 +1,29 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD-yxxre4gSYxlOdOajqRruip82xwz9_aQ",
+//   authDomain: "urbanium-8db16.firebaseapp.com",
+//   projectId: "urbanium-8db16",
+//   storageBucket: "urbanium-8db16.appspot.com",
+//   messagingSenderId: "445154266400",
+//   appId: "1:445154266400:web:225edda3f8d6a759c59b45",
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// ----------------------------------------------------------------------------------
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-yxxre4gSYxlOdOajqRruip82xwz9_aQ",
-  authDomain: "urbanium-8db16.firebaseapp.com",
-  projectId: "urbanium-8db16",
-  storageBucket: "urbanium-8db16.appspot.com",
-  messagingSenderId: "445154266400",
-  appId: "1:445154266400:web:225edda3f8d6a759c59b45",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
